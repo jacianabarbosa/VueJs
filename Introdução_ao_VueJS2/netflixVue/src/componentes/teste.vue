4825a6a2bff51b932823af19ab03cb9be52a4048
jacianabarbosas <jacianabarbosas@gmail.com>
2017-11-07 15:20:32 +0000

    .

---
+++ Introdução_ao_VueJS2/netflixVue/src/componentes/teste.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{titulo}}</h1>
+</template>
+
+
+
+<script>
+    export default{
+        props:['titulo']
+    }
+</script>
+
+<style lang="scss">
+h1{
+    color: red;
+}
+
+h3{
+    color: red;
+}
+
+</style>
