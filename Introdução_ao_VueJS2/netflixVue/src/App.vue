1d91ff2542f207a32995d7f55cc69d714f7b2f28
jacianabarbosas <jacianabarbosas@gmail.com>
2017-11-11 11:03:42 +0000

    .

--- Introdução_ao_VueJS2/netflixVue/src/App.vue
+++ Introdução_ao_VueJS2/netflixVue/src/App.vue
@@ -1,129 +1,22 @@
- <template>
+<template>
   <div class="contain">
-    <div class="slider">
-      <teste titulo="Categoria"></teste>
-      <h3>Ação</h3>
-      <span v-on:mouseover="scrollEsquerda()" v-on:mouseout="clearScroll()" class="handle handlePrev active">
-        <i class="fa fa-caret-left" aria-hidden="true"></i>
-      </span>
-
-      <div id="scroller" class="row">
-        <div class="row__inner">
-          <div class="gui-card">
-            <div class="gui-card__media">
-              <img class="gui-card__img" src="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700" alt=""  />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                Assassin’s Creed
-              </div>
-            </div>
-          </div>
-          <div class="gui-card">
-            <div class="gui-card__media">
-              <img class="gui-card__img" src="https://cdn.flickeringmyth.com/wp-content/uploads/2017/09/Thor-Ragnarok-banner-3-1-600x372.jpg" alt=""  />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                Thor Ragnarok
-              </div>
-            </div>
-          </div>
-          <div class="gui-card">
-            <div class="gui-card__media">
-              <img class="gui-card__img" src="https://s3.drafthouse.com/images/made/batmanvsuperman-xlarge_1050_591_81_s_c1.jpg" alt=""  />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                Batmam versus Superman
-              </div>
-            </div>
-          </div>
-          <div class="gui-card">
-            <div class="gui-card__media">
-              <img class="gui-card__img" src="https://www.blahcultural.com/wp-content/uploads/2017/06/mulher-maravilha-trailer-em-batalha-0-800x500.jpg" alt=""  />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                Mulher Maravilha
-              </div>
-            </div>
-          </div>
-          <div class="gui-card">
-            <div class="gui-card__media">
-              <img class="gui-card__img" src="https://static1.squarespace.com/static/51b3dc8ee4b051b96ceb10de/t/58c96236bf629a487883c763/1489592890396/" alt=""  />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                Logan
-              </div>
-            </div>
-          </div>
-          <div class="gui-card">
-            <div class="gui-card__media">
-              <img class="gui-card__img" src="http://cinemacomrapadura.com.br/imagens/2017/06/20170608-the-mummy-a-mumia-2017-tom-cruisi.jpg" alt=""  />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                A Múmia
-              </div>
-            </div>
-          </div>
-          <div class="gui-card">
-            <div class="gui-card__media">
-              <img class="gui-card__img" src="http://www.oquerola.com/wp-content/uploads/2015/02/Imagem-do-Homem-Aranha-1024x640.jpg" alt=""  />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                Homem-Aranha
-              </div>
-            </div>
-          </div>
-          <div class="gui-card">
-            <div class="gui-card__media">
-              <img class="gui-card__img" src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/02/03/15/transformers-the-last-knight.jpg" alt=""  />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                Transformers: O último cavaleiro
-              </div>
-            </div>
-          </div>
-          <div class="gui-card">
-            <div class="gui-card__media">
-              <img class="gui-card__img" src="http://www.recifefm.com.br/wp-content/uploads/2017/06/piratas-do-caribe.jpg" alt=""  />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                Assassin’s Creed
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </div>
-
-      <span v-on:mouseover="scrollDireita()" v-on:mouseout="clearScroll()"  class="handle handleNext active">
-        <i class="fa fa-caret-right" aria-hidden="true"></i>
-      </span>
-
-    </div>
+    <categoria titulo="Comédia"></categoria>
   </div>
 
 </template>
 
 <script>
-  import teste from './componentes/teste.vue';
-
+  // import teste from './componentes/teste.vue';
+  import Categoria from './componentes/Categoria.vue'
 export default {
-  name: 'app',
   components:{
-    teste
+    Categoria
   },
   data () {
     return {
       nomeProjeto: 'Netflix com Vue',
-      intervalo: null
+      intervalo: null,
+      
     }
   },
   methods:{
