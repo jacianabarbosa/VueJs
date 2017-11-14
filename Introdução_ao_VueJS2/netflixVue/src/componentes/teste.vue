3c61b68ca2fb958149e9682ac5efcc3e51ed3bc5
jacianabarbosa <jacianabarbosas@gmail.com>
2017-11-14 16:46:06 -0300

    Refiz o teste.vue e removi a categoria do componente

--- Introdução_ao_VueJS2/netflixVue/src/componentes/teste.vue
+++ Introdução_ao_VueJS2/netflixVue/src/componentes/teste.vue
@@ -1,23 +1,18 @@
 <template>
   <h1>{{titulo}}</h1>
+
 </template>
 
-
-
 <script>
-    export default{
-        props:['titulo']
-    }
+export default{  
+  props:['titulo']
+}
+
 </script>
 
 <style lang="scss">
 h1{
-    color: red;
+  color: red;
 }
-
-h3{
-    color: red;
-}
-// jaci comentou
 
 </style>
