6f0304c1d59e0f0e2e2f9961a83c5008ab69f552
jacianabarbosa <jacianabarbosas@gmail.com>
2017-11-15 22:15:13 -0300

    rotas personalizadas com vue. final do curso.

--- Introdução_ao_VueJS2/netflixVue/src/main.js
+++ Introdução_ao_VueJS2/netflixVue/src/main.js
@@ -2,27 +2,37 @@ import Vue from 'vue'
 // import App from './App.vue'
 import VueResource from 'vue-resource'
 Vue.use(VueResource)
-import Home from './paginas/Home.vue'
 
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import Home from './paginas/Home.vue'
 const Contato = { template: '<p>Página de Contato</p>' }
 const SemPagina = { template: '<p>Not Found</p>' }
 
-const rotas = {
-    '/': Home,
-    '/contato': Contato
+const routes = [
+    { path: '/', component: Home },
+    { path: '/contato', component: Contato }
 
-}
+]
 
-new Vue({
-    el: '#app',
-    data: {
-        rotaAtual: window.location.pathname
-    },
-
-    computed: {
-        exibeComponente() {
-            return rotas[this.rotaAtual] || SemPagina
-        }
-    },
-    render(h) { return h(this.exibeComponente) }
+const router = new VueRouter({
+    routes
 })
+
+// new Vue({
+//     el: '#app',
+//     data: {
+//         rotaAtual: window.location.pathname
+//     },
+//     computed: {
+//         exibeComponente() {
+//             return rotas[this.rotaAtual] || SemPagina
+//         }
+//     },
+//     render(h) { return h(this.exibeComponente) }
+// })
+
+const app = new Vue({
+    router
+}).$mount('#app')
